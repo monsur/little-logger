cd2fee0d94223553f2156e5a81dddcdc981b4984
Monsur Hossain <monsur@gmail.com>
2013-02-26 16:32:45 -0600

    Adding file with examples.

---
+++ example.js
@@ -0,0 +1,16 @@
+var logger = require('./little-logger');
+
+var l = new logger.Logger();
+l.info('The default log level is "info".');
+
+l = new logger.Logger('debug');
+l.info('This logger will log any message at level DEBUG or higher.');
+l.info('Supports four log levels:');
+l.debug('\tDEBUG');
+l.info('\tINFO');
+l.warn('\tWARN');
+l.error('\tERROR');
+
+l = new logger.Logger('debug', {color: false});
+l.info('By default, output is colorized.');
+l.warn('But this can be turned off.');
