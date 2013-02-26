b9d0868c612df42b4340c076ed3d5d6282109eae
Monsur Hossain <monsur@gmail.com>
2013-02-26 16:41:29 -0600

    Adding more examples

--- example.js
+++ example.js
@@ -17,3 +17,16 @@ l.warn('But this can be turned off.');
 
 l = new logger.Logger('info', {format: '%a'});
 l.info('The log format can be changed.');
+
+l = new logger.Logger('info', {format: '%m/%d/%Y %H:%M:%S.%f %a', utc: true});
+l.info('There are fine-grained controls for displaying the date.');
+l.info('And times can be either local or UTC.');
+
+l = new logger.Logger('info', {writer: console.error});
+l.info('Logs to stdio by default');
+l.info('But can log to stderr as well');
+
+l = new logger.Logger('info', {writer: function(msg) {
+  console.log(msg + ' (this message is logged from a custom logger)');
+}});
+l.info('You can even write your own logger function.');
