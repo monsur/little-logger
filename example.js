cb7f9e10cb4308f46b1a3d61cd55a767613e7cfa
Monsur Hossain <monsur@gmail.com>
2013-02-26 23:12:01 -0600

    tweaking example.

--- example.js
+++ example.js
@@ -8,10 +8,10 @@ l.info('The default log level is "info".');
 l = new logger.Logger('debug');
 l.info('This logger will log any message at level DEBUG or higher.');
 l.info('Supports four log levels:');
-l.debug('\tDEBUG');
-l.info('\tINFO');
-l.warn('\tWARN');
-l.error('\tERROR');
+l.debug('DEBUG');
+l.info('INFO');
+l.warn('WARN');
+l.error('ERROR');
 
 l = new logger.Logger('debug', {color: false});
 l.info('By default, output is colorized.');
