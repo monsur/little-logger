cb3f44bcf22267d12ee5df8b9cb8efc435914d4c
Monsur Hossain <monsur@gmail.com>
2013-02-28 13:48:02 -0600

    Switch from using replace() to iterating over the string.

--- little-logger.js
+++ little-logger.js
@@ -4,7 +4,7 @@ var pad = function(p, v) {
   return padLength > -1 ? Array(padLength+1).join('0') + v : v;
 };
 
-var getDateFunctions = function(utc) {
+var getFormatFunctions = function(utc) {
   var getf = function(name, padding) {
     return function(d) {
       var val = d[name].call(d);
@@ -14,14 +14,17 @@ var getDateFunctions = function(utc) {
   };
   var utcStr = utc ? 'UTC' : '';
   return {
-    '%D': getf('to' + utcStr + 'String'),
-    '%Y': getf('get' + utcStr + 'FullYear'),
-    '%m': function(d) { return pad(2, d.getMonth() + 1); },
-    '%d': getf('get' + utcStr + 'Date', 2),
-    '%H': getf('get' + utcStr + 'Hours', 2),
-    '%M': getf('get' + utcStr + 'Minutes', 2),
-    '%S': getf('get' + utcStr + 'Seconds', 2),
-    '%f': getf('get' + utcStr + 'Milliseconds', 3),
+    'D': getf('to' + utcStr + 'String'),
+    'Y': getf('get' + utcStr + 'FullYear'),
+    'm': function(d) { return pad(2, d.getMonth() + 1); },
+    'd': getf('get' + utcStr + 'Date', 2),
+    'H': getf('get' + utcStr + 'Hours', 2),
+    'M': getf('get' + utcStr + 'Minutes', 2),
+    'S': getf('get' + utcStr + 'Seconds', 2),
+    'f': getf('get' + utcStr + 'Milliseconds', 3),
+    '%': function(d) { return '%'; },
+    'l': function(d, l, a) { return l; },
+    'a': function(d, l, a) { return a; }
   }
 };
 
@@ -33,7 +36,7 @@ var Logger = exports.Logger = function(level, options) {
   options.format = options.format || '%Y-%m-%d %H:%M:%S.%f %l: %a';
   options.writer = options.writer || console.log;
   this.options = options;
-  this.dateFunctions = getDateFunctions(this.options.utc);
+  this.formatFunctions = getFormatFunctions(this.options.utc);
 };
 
 Logger.LOG_LEVELS = {
@@ -52,27 +55,37 @@ Logger.prototype.log = function(level, msg) {
   var msg_val = Logger.LOG_LEVELS[level.toUpperCase()];
   var log_val = Logger.LOG_LEVELS[this.level_key.toUpperCase()];
   var date = new Date();
-  var msg_ = this.options.format;
-  for (var format in this.dateFunctions) {
-    if (msg_.indexOf(format) > -1) {
-      msg_ = msg_.replace(format, this.dateFunctions[format].call({}, date));
+  var buff = [];
+  for (var i = 0, len = this.options.format.length; i < len; i++) {
+    var c1 = this.options.format[i];
+    if (c1 === '%' && i < len - 1) {
+      var c2 = this.options.format[++i];
+      if (c2 in this.formatFunctions) {
+        buff.push(this.formatFunctions[c2].call({}, date, level, msg));
+      } else {
+        buff.push(c1);
+        buff.push(c2);
+      }
+    } else {
+      buff.push(c1);
     }
   }
-  msg_ = msg_.replace('%l', level).replace('%a', msg);
   if (this.options.color && msg_val.color) {
-    msg_ = msg_val.color + msg_ + '\033[0m';
+    buff.unshift(msg_val.color);
+    buff.push('\033[0m');
   }
+  var formattedMsg = buff.join('');
   if (msg_val.value >= log_val.value) {
     var writer = msg_val['writer'] || this.options.writer;
     var args = [].splice.call(arguments,0).splice(2);
-    args.unshift(msg_);
+    args.unshift(formattedMsg);
     writer.apply(this, args);
   }
   return {
     date: date,
     level: level,
     message: msg,
-    formattedMessage: msg_
+    formattedMessage: formattedMsg
   };
 };
 
